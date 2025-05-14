077ed7d579b9177b26a5e64f7739fed01979c511
random2907 <yarnjeetyadav1234567890@gmail.com>
2025-05-14 13:17:48 +0530

    its working

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import time
+import copy
 
-
-def matrix(row: int, col: int):
+def matrix(row: int, col: int) -> list[list[int]]:
     return [[0] * col for _ in range(row)]
 
 
@@ -11,82 +11,70 @@ bg_black = '\033[38;2;0;0;0m'
 clear = '\033[2J\033[H'
 
 
-def sim_start(l: list[list[int]]):
+def sim_start(l: list[list[int]]) -> None:
     while True:
         print(clear, end="")
         for i in l:
             for j in i:
                 if j == 0:
-                    print(bg_white + '#'+reset, end=" ")
+                    # print(bg_white + '#'+reset, end=" ")
+                    print(bg_white + '█' + reset, end=" ")  # dead
+
                 else:
-                    print(bg_black + '#'+reset, end=" ")
+                    # print(bg_black + '#'+reset, end=" ")
+                    print(bg_black + '█' + reset, end=" ")  # alive
             print()
+        l=conway_rules(l)
         time.sleep(1)
 
 
-def calculate_neighbours(l: list[list[int]]):
-    full_neighbours = []
-    for row_idx, row in enumerate(l):
-        for col_idx, value in enumerate(row):
-            if value == 1:
-                neighbours = {}
-                neighbours["alive"] = True
-                neighbours["row"] = row_idx
-                neighbours["col"] = col_idx
-                neighbours["neighbour"] = [
-                    [row_idx, col_idx + 1],
-                    [row_idx, col_idx - 1],
-                    [row_idx + 1, col_idx],
-                    [row_idx - 1, col_idx],
-                    [row_idx + 1, col_idx - 1],
-                    [row_idx - 1, col_idx + 1],
-                    [row_idx + 1, col_idx + 1],
-                    [row_idx - 1, col_idx - 1],
-                ]
-                full_neighbours.append(neighbours)
-    return full_neighbours
+def count_neighbours(l: list[list[int]], r: int, c: int) -> int:
+    directions = [ (-1, 1),
+                  (0, 1),
+                  (1, 0),
+                  (1, 1),
+                  (0, -1),
+                  (1, -1),
+                  (-1, -1),
+                  (-1, 0)
+                  ]
+    count = 0
+    for x, y in directions:
+        nextx, nexty = r+ x, c+ y
+        if 0 <= nextx < len(l) and 0 <= nexty < len(l[0]) and l[nextx][nexty] == 1:
+            count+=1
+    return count
 
 
-def conway_rules(l: list[list[int]]):
-    #    Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    #    Any live cell with two or three live neighbours lives on to the next generation.
-    #    Any live cell with more than three live neighbours dies, as if by overpopulation.
-    #    Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-    neighbours = calculate_neighbours(l)
-    for cell in neighbours:
-        print("before: ",cell)
-        count = 0
-        for neighbour in cell["neighbour"]:
-            if l[neighbour[0]][neighbour[1]] == 1:
-                count+=1
+def conway_rules(l: list[list[int]]) -> list[list[int]]:
+    rows, cols = len(l), len(l[0])
+    temp = copy.deepcopy(l)
 
-        if cell["alive"]:
-            if (count < 2 or count > 3):
-                print("death")
-                l[cell["row"]][cell["col"]] = 0
-            else: 
-                l[cell["row"]][cell["col"]] = 1
-            for dead_neighbour in 
-                if count==3:
-                    print("birth")
-                    l[cell["row"]][cell["col"]] = 1
+    for r in range(rows):
+        for c in range(cols):
+            neighbors = count_neighbours(l, r, c)
+            if l[r][c] == 1:
+                # live cell rules
+                if neighbors < 2 or neighbors > 3:
+                    temp[r][c] = 0
+                else:
+                    temp[r][c] = 1
+            else:
+                # dead cell rule
+                if neighbors == 3:
+                    temp[r][c] = 1
 
-    neighbours = calculate_neighbours(l)
-    for cell in neighbours:
-        print("after: ",cell)
+    return temp
 
-
-def conway_start():
+def conway_start() -> None:
     l = matrix(100, 100)
     l[50][52] = 1
     l[51][53] = 1
     l[52][51] = 1
     l[52][52] = 1
     l[52][53] = 1
-    conway_rules(l)
-
-    # sim_start(l)
+    sim_start(l)
 
 
 if __name__ == "__main__":
